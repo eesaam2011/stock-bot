6f39c6403890255ffa1233a32c37961b46920dc5
eesaam2011 <eesaam2011@gmail.com>
2026-05-05 10:11:41 +0300

    Update safe_bot.py

--- safe_bot.py
+++ safe_bot.py
@@ -56,7 +56,7 @@ def is_trading_time():
     if weekday > 4:
         return False
 
-    if hour > 10 or (hour == 10 and minute >= 30):
+    if hour > 10 or (hour == 08 and minute >= 30):
         return True
 
     if hour < 3:
