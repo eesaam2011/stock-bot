3844e5e873b52867b416c767edf21ded4992faa2
eesaam2011 <eesaam2011@gmail.com>
2026-05-12 20:48:20 +0300

    Update master_list.py

--- master_list.py
+++ master_list.py
@@ -45,7 +45,7 @@ BLACKLIST_SYMBOLS = [
     "PM", "MO",
     "CGC", "TLRY", "ACB",
     "NCLH", "CCL", "RCL",
-    "AMC", "GPRE", "SKLZ", "PGY", "JELD", "TWO",
+    "AMC", "GPRE", "SKLZ", "PGY", "JELD", "TWO", "PGEN",
 ]
 
 BLACKLIST_KEYWORDS = [
