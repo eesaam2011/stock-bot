f3686ce9f6a1141b35790f6d23c0d8b4a7f72aed
eesaam2011 <eesaam2011@gmail.com>
2026-06-01 19:35:56 +0300

    Update bot3_time_machine_test.py

--- bot3_time_machine_test.py
+++ bot3_time_machine_test.py
@@ -2052,7 +2052,7 @@ def check_ready_entry(symbol, data, df=None):
 
         ignition_quality = (
             volume_acceleration
-            and close_position >= 0.70
+            and close_position >= 0.78
             and upper_wick_pct <= 0.35
             and distribution_score < 22
             and (
