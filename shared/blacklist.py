48e9f40c5ab6cf2f489a30b2141304ef7aecb2ee
eesaam2011 <eesaam2011@gmail.com>
2026-06-03 09:41:45 +0300

    Create blacklist.py

---
+++ shared/blacklist.py
@@ -0,0 +1,22 @@
+SYMBOL_BLACKLIST = {
+    "JPM", "BAC", "WFC", "C", "GS", "MS",
+    "DKNG", "PENN", "WYNN", "LVS",
+    "BUD", "STZ", "DEO",
+    "PM", "MO",
+    "CGC", "TLRY", "ACB",
+    "NCLH", "CCL", "RCL",
+    "AMC", "GPRE", "SKLZ", "PGY", "JELD",
+    "TWO", "PGEN", "GENI", "TRC",
+}
+
+BAD_KEYWORDS = [
+    "bank", "insurance", "credit", "lending", "mortgage", "pawn",
+    "casino", "gambling", "betting", "sportsbook",
+    "alcohol", "beer", "wine",
+    "tobacco",
+    "cannabis", "marijuana", "hemp",
+    "cruise",
+    "adult",
+    "etf", "fund", "trust",
+    "warrant", "unit",
+]
