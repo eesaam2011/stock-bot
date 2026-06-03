71adfe0007c3591e1a74f82ecf2fda9b04998e70
eesaam2011 <eesaam2011@gmail.com>
2026-06-03 12:08:32 +0300

    Update alpaca_assets.py

--- market_sources/alpaca_assets.py
+++ market_sources/alpaca_assets.py
@@ -1,24 +1,10 @@
-import os
-import alpaca_trade_api as tradeapi
+from market_sources.alpaca_client import api
 from hunter.hunter_filters import (
     is_clean_symbol,
     has_bad_keywords,
 )
-
 from shared.blacklist import SYMBOL_BLACKLIST
 
-API_KEY = os.getenv("APCA_API_KEY_ID")
-SECRET_KEY = os.getenv("APCA_API_SECRET_KEY")
-BASE_URL = os.getenv(
-    "APCA_API_BASE_URL",
-    "https://paper-api.alpaca.markets",
-)
-
-api = tradeapi.REST(
-    API_KEY,
-    SECRET_KEY,
-    BASE_URL,
-)
 # =========================================
 # Layer 0 Asset Loader
 # =========================================
