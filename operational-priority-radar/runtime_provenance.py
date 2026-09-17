cb6104f32266881d7150afb0f48bf77169ee7ef3
eesaam2011 <eesaam2011@gmail.com>
2026-09-17 20:02:13 +0300

    Delete operational-priority-radar directory

--- operational-priority-radar/runtime_provenance.py
+++
@@ -1,9 +0,0 @@
-RUNTIME_MODE="SHADOW"
-SHADOW_MODE=True
-RELEASE_ID="OPERATIONAL_V1_STEP16G_SHADOW_PROVENANCE_2026-09-17"
-def stamp(record):
- r=dict(record)
- r["shadow_mode"]=SHADOW_MODE
- r["runtime_mode"]=RUNTIME_MODE
- r["release_id"]=RELEASE_ID
- return r
